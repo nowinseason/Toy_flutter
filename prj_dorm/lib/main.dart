c5c07b911db27417342145d0c8a8c5d70b68ec06
nowinseason <nowinseeson@gmail.com>
2023-08-03 07:27:54 -0500

    connect_fb

--- prj_dorm/lib/main.dart
+++ prj_dorm/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:prj_dorm/responsive/mobile_screen_layout.dart';
 import 'package:prj_dorm/responsive/responsive_layout_screen.dart';
 import 'package:prj_dorm/responsive/web_screen_layout.dart';
 import 'package:prj_dorm/util/colors.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
---
+++ prj_dorm/lib/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+class LoginScreeen extends StatefulWidget {
+  const LoginScreeen({super.key});
+
+  @override
+  State<LoginScreeen> createState() => _LoginScreeenState();
+}
+
+class _LoginScreeenState extends State<LoginScreeen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('From Login Screen '),
+    );
+  }
+}
